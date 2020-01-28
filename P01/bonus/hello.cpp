6f39fb65686f16edc90652c226b94a334b04f48b
jasonshamayev <jason.shamayev@mavs.uta.edu>
2020-01-27 21:47:14 -0600

    bonus

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    string name;
+
+    cout << "What is your name?: ";
+
+    getline(cin, name);
+
+    cout<<"Hello, " << name << "!" << endl;
+
+
+
+    return 0;
+
+}
+
