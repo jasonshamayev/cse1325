4555857d07016951ba2a87ad471166fff91cd1b8
jasonshamayev <jason.shamayev@mavs.uta.edu>
2020-02-03 15:58:48 -0600

    added main.cpp

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
-#
+#include "color.h"
 
+
+Color::Color(int red, int green, int blue){
+	_red = red;
+	_green = green;
+	_blue = blue;
+
+}
+
+std::string Color::to_string() {
+		return "(" + std::to_string(red) + ","
+             		+ std::to_string(green) + ","
+            		 + std::to_string(blue) + ")";}
+		
+
+
+
+}
+std::string colorize(std::string text){
+
+
+
+}
--- P02/full_credit/color.h
+++ P02/full_credit/color.h
@@ -4,26 +4,24 @@
 
 
 class Color {
+
+	public: 
+		Color(); // default Constructor
+		
+		std::string colorize(std::string text); 
+		std::string to_string(); // returns a string representation of the RGB color in format "(r,g,b)"
+
+       		 /*return "(" + std::to_string(red) + ","
+             		+ std::to_string(green) + ","
+            		 + std::to_string(blue) + ")";}
+		std::string colorize(std::string text);
+		*/
+		
 	private: // red green blue (int)
 		int _red, _green, _blue;
 		
 		if (0 > _red || _red > 255) throw 			std::runtime_error{"Invalid color input"};
 
-
-
-
-	public: 
-		Color(int red, int green, int blue);
-		
-		std::string colorize(std::string text); //colorize(text: string): string
-		std::string to_string(){
-{
-       		 return "(" + std::to_string(red) + ","
-             		+ std::to_string(green) + ","
-            		 + std::to_string(blue) + ")";}
-		std::string colorize(std::string text);
-		//magnitude() : int
-	       // int compare(const Color& rhs)
 
 
 };
