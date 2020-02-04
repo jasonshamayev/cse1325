48f00eabdbd222ac1160b2cf2720feb4f9d51044
jasonshamayev <jason.shamayev@mavs.uta.edu>
2020-02-03 20:23:33 -0600

    working test file

--- P02/full_credit/test.cpp
+++ P02/full_credit/test.cpp
@@ -6,6 +6,20 @@
 #include <iostream>
 
 int main() {
+	int r,g,b;
     Color red{255,0,0};
     std::cout << red.colorize("Red") << std::endl;
+Color blue{0,0,255};
+    std::cout << blue.colorize("Blue") << std::endl;
+
+    Color green{0,255,0};
+   std::cout << green.colorize("Green") << std::endl;
+
+    std::cout << "Enter 3 numbers representing red, green, and blue" << std::endl;
+    std::cin >> r; 
+    std::cin >> g;
+    std::cin >> b;
+    Color color{r,g,b};
+    std::cout << color.colorize(color.to_string()) << std::endl;
+
 }
