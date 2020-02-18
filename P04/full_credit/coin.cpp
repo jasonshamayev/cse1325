93936f00006a76e12666c73518b4307345e63392
jasonshamayev <60237556+jasonshamayev@users.noreply.github.com>
2020-02-17 21:42:04 -0600

    adjustments
    adjustments

--- P04/full_credit/coin.cpp
+++ P04/full_credit/coin.cpp
@@ -2,24 +2,33 @@
 #include <iostream>;
 #include <string>;
 
-Coin::Coin(Size size, Year year){
+Coin::Coin(Coin_size size, Year year){
 	_size = size;
 	_year = year;
 	_notes = nullptr;
 }
 
-Coin:Coin(){
+
+Coin::Coin(){
 	
+	_size;
+	_year = 0;
+	_notes = nullptr;
 
 
 }
 ~Coin() {delete _notes;}
 
+Coin& operator=(const Coin& rhs) {
+	if (this != &rhs) _notes = new std::string * rhs(notes);
+	return *this;
+} // Copy Assignment
 
-std::ostream& operator<<(std::ostream& ost, const Coin& coin){
+std::ostream& operator<<(std::ostream& ost, const Coin& coin) {
 
 	ost << coin._year + " " + "";
 	return ost;
+}
 Coin::add_note(std::string s){
 	if(!_notes){
 
--- P04/full_credit/coin.h
+++ P04/full_credit/coin.h
@@ -1,39 +1,40 @@
 #ifndef __COIN_H
 #define __COIN_H
 #include <string>
+#include <iostream>
+#include <ostream>
 
-enum class Coin_size   {Penny, Nickel, Dime, Quarter};
+enum class Coin_size   {PENNY, NICKEL, DIME, QUARTER};
 
-	std::string to_string(Coin coin){
-		switch(coin){
-		case Coin::Penny: return "Penny"; break;
-		case Coin::Nickel: return "Nickel"; break;
-		case Coin::Dime: return "Dime"; break;
-		case Coin::Quarter: return "Quarter"; break;
-		default: throw std::runtime_error{Invalid coin};
+std::string to_string(Coin_size coin) {
+	switch (coin) {
+	case Coin_size::PENNY: return "Penny"; break;
+	case Coin_size::NICKEL: return "Nickel"; break;
+	case Coin_size::DIME: return "Dime"; break;
+	case Coin_size::QUARTER: return "Quarter"; break;
+	default: throw std::runtime_error{ "Invalid coin" };
 
+	}
 }
 
-class Coin{
-	typedef int Year;
-	public:
-		Coin(); // Default Constructor
-		Coin(Size size, Year year);
-		Coin(const Coin& rhs) // Copy Constructor: _Year{rhs.Year()} {}; // Copy Constructor
-		~Coin(){delete _year}; // Destructor
-		int* get() const {return _year;}
-		void set(int* year) {*_year = *year;}
-		Coin& operator=(const Coin& rhs){
-		if(this != &rhs) _year = new int rhs.year();
-		return *this;
-} // Copy Assignment
+		class Coin {
+			typedef int Year;
+		public:
+			Coin(); // Default Constructor
+			Coin(Coin_size size, Year year);
+			Coin(const Coin& rhs); // Copy Constructor: _Year{rhs.Year()} {}; // Copy Constructor;
+			~Coin() { delete _notes}; // Destructor
+			std::string* get() const { return _notes; }
+			void set(std::string* notes) { *_notes = *notes; }
+			Coin& operator=(const Coin& rhs);
+			/*{
+				if (this != &rhs) _notes = new std::string* rhs(notes);
+				return *this;
+			}  Copy Assignment */
 
-		friend std::ostream& operator<<(std::ostream& ost, const Coin&);// 
-		friend std::istream& operator>>(std::istream& ist, Coin&);
-		// 
-		Coin::add_note(std::string s);
-
-
+			friend std::ostream& operator<<(std::ostream& ost, const Coin&);// 
+			friend std::istream& operator>>(std::istream& ist, Coin&); //
+			void add_note(std::string s);
 
 
 
@@ -44,11 +45,15 @@ class Coin{
 
 
 
-	private:
-		Coin::_size = _size;
-		Year = _year;
-		std::string* = _notes;
-	
 
 
-}
+		private:
+			Coin_size _size;
+			Year _year;
+			std::string* _notes;
+
+
+
+		};
+
+#endif
